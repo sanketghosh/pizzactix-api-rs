fbee5b5e741638d42e3cf501d6adf7b0c2439e55
sanket <code.sanket13@gmail.com>
2024-02-12 00:41:39 +0530

    add: connection with surrealdb

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-use actix_web::{get, patch, post, web::Json, web::Path, App, HttpResponse, HttpServer, Responder};
+use actix_web::{
+    get, patch, post, web::Data, web::Json, web::Path, App, HttpResponse, HttpServer, Responder,
+};
 use std::io::Result;
+// use surrealdb::sql::Data;
 mod models;
 use crate::models::pizza::{BuyPizzaRequest, UpdatePizzaURL};
 use validator::Validate;
+mod db;
+use crate::db::Database;
 
 // handlers
 #[get("/pizzas")]
@@ -31,8 +36,15 @@ async fn update_pizza(update_pizza_url: Path<UpdatePizzaURL>) -> impl Responder 
 // main
 #[actix_web::main]
 async fn main() -> Result<()> {
-    HttpServer::new(|| {
+    let db = Database::init()
+        .await
+        .expect("ERROR: cannot connect to the database");
+
+    let db_data = Data::new(db);
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(db_data.clone())
             .service(get_pizzas)
             .service(post_pizza)
             .service(update_pizza)
