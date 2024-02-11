c51ff6780e38c86880c32f76fb38650cd56b6130
sanket <code.sanket13@gmail.com>
2024-02-11 20:52:32 +0530

    add: buypizza endpoint complete

--- README.md
+++ README.md
@@ -9,5 +9,4 @@ A simplest pizza shop api using Rust's Actix Web with SurrealDB.
 ## Requirements:
 
 1. Make sure to have Rust and SurrealDB installed in your machine.
-
-2. Package installation: `cargo add actix-web json serde surrealdb uuid validator async-trait derive_more -F serde/derive -F validator/derive`
+2. Crates installation: `cargo add actix-web json serde surrealdb uuid validator async-trait derive_more -F serde/derive -F validator/derive`
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, patch, post, web::Json, App, HttpResponse, HttpServer, Responder};
+use std::io::Result;
+mod models;
+use crate::models::pizza::BuyPizzaRequest;
+use validator::Validate;
+
+// handlers
+#[get("/pizzas")]
+async fn get_pizzas() -> impl Responder {
+    HttpResponse::Ok().body("Pizzas available")
+}
+
+#[post("/buypizza")]
+async fn post_pizza(body: Json<BuyPizzaRequest>) -> impl Responder {
+    let is_valid = body.validate();
+    match is_valid {
+        Ok(_) => {
+            let pizza_name = body.pizza_name.clone();
+            HttpResponse::Ok().body(format!("pizza entered is {pizza_name}"))
+        }
+        Err(_) => HttpResponse::Ok().body("Pizza name required"),
+    }
+}
+
+#[patch("/updatepizza/{id}")]
+async fn update_pizza() -> impl Responder {
+    HttpResponse::Ok().body("Updating a pizza")
+}
+
+// main
+#[actix_web::main]
+async fn main() -> Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(get_pizzas)
+            .service(post_pizza)
+            .service(update_pizza)
+    })
+    .bind("127.0.0.1:6969")?
+    .run()
+    .await
 }
